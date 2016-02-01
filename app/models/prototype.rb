f169b41275526a733e92ff0bdca2d0d3c66966c8
TakeumiYamamura <yama1223xxx@gmail.com>
2016-02-01 20:15:04 +0900

    add status column and association

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,7 +2,10 @@ class Prototype < ActiveRecord::Base
   has_many :images
   has_many :likes
   has_many :tags
+  belongs_to :user
 
   accepts_nested_attributes_for :images
+
+  acts_as_taggable
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201105360) do
+ActiveRecord::Schema.define(version: 20160201111433) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.integer  "status",       limit: 4
   end
 
   add_index "images", ["prototype_id"], name: "index_images_on_prototype_id", using: :btree
