8f06bb5cbbd4818b67b5e521fce98b3b581f9ba7
TakeumiYamamura <yama1223xxx@gmail.com>
2016-02-02 11:46:57 +0900

    fix likes bag

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,17 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :set_prototype, only: [:create, :destroy]
 
   def create
-    @prototype = Prototype.find(params[:prototype_id])
     @prototype.likes.create(user_id: current_user.id)
   end
 
   def destroy
     like = Like.find(params[:id])
-    @prototype = like.prototype
     like.destroy
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
